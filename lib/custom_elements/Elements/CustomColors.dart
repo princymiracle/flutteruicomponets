326126675e5ef7fb810ab020ba0b2884ca6e8698
princymiracle <155509508+princymiracle@users.noreply.github.com>
2024-01-08 15:44:18 +0530

    init commit

---
+++ lib/custom_elements/Elements/CustomColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  static const appbarcolor = Colors.cyan;
+  static const buttoncolor1 = Colors.blueGrey;
+  static const buttoncolor2 = Colors.white;
+  static const text = Colors.black;
+}
